a69e407ce0eec520acae7c0b1a6cd0b90b9b4db7
SebastianCuoghi2008 <sebastian.cuoghi@galileiostiglia.edu.it>
2025-09-16 07:55:01 +0000

    15092025_1°Esercizio

--- Array/15092025_1°Esercizio.c
+++ Array/15092025_1°Esercizio.c
@@ -1,43 +1,51 @@
 /*Data una matrice di valori scambiare gli elementi della diagonale principale con la diagonale secondaria.*/
 
 #include <stdio.h>
-#include "libArray.c"
-#define Righe 10
-#define Colonne 8
+#include <stdlib>
+#include <time.h>
+#define N 4
+
+void CaricaMatrice(int (*_matrice)[N]){
+    printf("Carichiamo la matrice: ");
+    for (int i=0; i<N; i++){
+        for(int j=0 j<N; j++){
+            _matrice[i][j]=rand()%10 + 1;
+        }
+    }
+}
+
+void StampaMatrice(int (*_matrice)[]){
+    for (int i=0; i<N; i++){
+        for(int j=0 j<N; j++){
+            printf("%d\t", _matrice[i][j]);
+            printf("\n");
+        }
+        printf("\n");
+    }    
+}
+
+void ScambioMatrice(int (*_matrice)[N]){
+    int temp;
+
+    for(int i=0; i<N; i++){
+        temp=_matrice[i][j];
+    }
+}
+
 
 int main(){
     srand(time(NULL));
-    int mat[Righe][Colonne];
+    int matrice[N][N]={0};
 
-    for(int i=0; i<Righe; i++){
-        for(int j=0; j<Colonne; j++){
-            mat[i][j] = i;
-        }
-    }
-    for(int i=0; i<Righe; i++){
-        for(int j=0; j<Colonne; j++){
-            printf("%d\t", mat[i][j]);
-        }
-        printf("\n");
-    }
-    riempiVettoreCasuale(mat[0], Colonne, 0, 20);
-    for(int i=0; i<Righe; i++){
-        for(int j=0; j<Colonne; j++){
-            printf("%d\t", mat[i][j]);
-        }
-        printf("\n");
-    }
-    for(int i=0; i<Righe; i++){
-        riempiVettoreCasuale(mat[i], Colonne, 0, 100);
-        stampaVettore(mat[i], Colonne, '\t');
-        bubbleSort(mat[i], Colonne);
-    }
-    printf("\n\n\n");
-    for(int i=0; i<Righe; i++){
-        for(int j=0; j<Colonne; j++){
-            printf("%d\t", mat[i][j]);
-        }
-        printf("\n");
-    }
-    return 0;
-}
+    //caricamento matrice
+    CaricaMatrice(matrice);
+
+    //stampa matrice
+    StampaMatrice(matrice);
+
+    //scambio matrice
+    ScambioMDiagonale(matrice);
+
+    //stampa matrice
+    StampaMatrice(matrice);
+}    
