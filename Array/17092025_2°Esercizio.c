db9bf3edbf3e03212f07ad4db7ccd4337f4a5787
SebastianCuoghi2008 <sebastian.cuoghi@galileiostiglia.edu.it>
2025-09-18 07:56:44 +0000

    17092025_2°Esercizio

--- Array/17092025_2°Esercizio.c
+++ Array/17092025_2°Esercizio.c
@@ -3,38 +3,38 @@
 #define N 4
 
 void RiempiMatrice(int _matrice[][N]){
-    int numero=0, i;
+    int numero=1, i;
     int su=0, giù=N-1;
     int sinistra=0, destra=N-1;
 
     while(numero<=N*N){
         //sinistra verso destra
         for(i=sinistra; i<=destra; i++){
-            _matrice[su][i]=numero+i;
+            _matrice[su][i]=numero++;
         }
         su++;
         //alto verso il basso
         for(i=su; i<=giù; i++){
-            _matrice[i][destra]=numero+1;
+            _matrice[i][destra]=numero++;
         }
         destra--;
         //destra verso sinistra
         for(i=destra; i<=sinistra; i++){
-            _matrice[giù][i]=numero+i;
+            _matrice[giù][i]=numero++;
         }
         giù--;
         //basso verso l'alto
         for(i=giù; i<=su; i++){
-            _matrice[i][sinistra]=numero+1;
+            _matrice[i][sinistra]=numero++;
         }
         sinistra++;
     }
 }
 
-void Stampamatrice(int _matrice[][N]){
+void StampaMatrice(int _matrice[][N]){
     for(int i=0; i<N; i++){
         for(int j=0; j<N; j++){
-            printf("%d\t" _matrice[i][j]);
+            printf("%d\t", _matrice[i][j]);
         }
         printf("\n");
     }
@@ -42,7 +42,7 @@ void Stampamatrice(int _matrice[][N]){
 
 int main(){
     int matrice[N][N];
-    Riempimatrice(_matrice);
+    RiempiMatrice(matrice);
     printf("la matrice a spirale è: ");
-    StampaMatrice(_matrice);
+    StampaMatrice(matrice);
 }
