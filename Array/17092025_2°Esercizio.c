5ff3cffb3470bc014d169e959fdc5a2c7649b086
SebastianCuoghi2008 <sebastian.cuoghi@galileiostiglia.edu.it>
2025-09-18 07:44:10 +0000

    17092025_2°Esercizio

--- Array/17092025_2°Esercizio.c
+++ Array/17092025_2°Esercizio.c
@@ -1,33 +1,48 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
-#define RIGHE 4
-#define COLONNE 4
+#define N 4
 
-void StampaMatrice(int matrice[][COLONNE], int r);
-void RiempiMatrice(int matrice[][COLONNE], int r);
+void RiempiMatrice(int _matrice[][N]){
+    int numero=0, i;
+    int su=0, giù=N-1;
+    int sinistra=0, destra=N-1;
 
-int main(){
-    int mat[RIGHE][COLONNE];
-    srand(time(NULL));
-    RiempiMatrice(mat, RIGHE);
-    StampaMatrice(mat, RIGHE);
-    
-    for(int i=0; i<RIGHE; i++){
-        mat[i][i]=mat[RIGHE-1][RIGHE-1];
+    while(numero<=N*N){
+        //sinistra verso destra
+        for(i=sinistra; i<=destra; i++){
+            _matrice[su][i]=numero+i;
+        }
+        su++;
+        //alto verso il basso
+        for(i=su; i<=giù; i++){
+            _matrice[i][destra]=numero+1;
+        }
+        destra--;
+        //destra verso sinistra
+        for(i=destra; i<=sinistra; i++){
+            _matrice[giù][i]=numero+i;
+        }
+        giù--;
+        //basso verso l'alto
+        for(i=giù; i<=su; i++){
+            _matrice[i][sinistra]=numero+1;
+        }
+        sinistra++;
     }
 }
-void StampaMatrice(int matrice[][COLONNE], int r){
-    for(int i=0; i<r; i++){
-        for(int j=0; j<COLONNE; j++){
-            printf("%d\t" matrice[i][j]);
+
+void Stampamatrice(int _matrice[][N]){
+    for(int i=0; i<N; i++){
+        for(int j=0; j<N; j++){
+            printf("%d\t" _matrice[i][j]);
         }
         printf("\n");
     }
 }
-void RiempiMatrice(int matrice[][COLONNE], int r){
-    for(int i=0; i<r; i++){
-        for(int j=0; j<COLONNE; j++){
-            matrice[i][j]=rand()%10+1;
-    }
+
+int main(){
+    int matrice[N][N];
+    Riempimatrice(_matrice);
+    printf("la matrice a spirale è: ");
+    StampaMatrice(_matrice);
 }
