b488d53ae7c0b0566124e9b1aed3d9f0a448aaf2
SebastianCuoghi2008 <sebastian.cuoghi@galileiostiglia.edu.it>
2025-10-21 07:54:35 +0000

    StringheMalloc2

---
+++ Stringhe/StringheMalloc2.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+typedef char* String;
+
+void compatta(String buffer, int len){
+    if(len>0 && buffer[len-1]=='\n'){
+        buffer[len-1]='\0';
+        len --;
+    }
+}
+
+int conteggio(String frase){
+    int cnt=0;
+
+    for(int i=0; frase[i]!='\0'; i++){
+        char c=tolower(frase[i]);
+        if(c=='a' || c=='e' || c=='i' || c=='o' || c=='u'){
+            cnt++;
+        }
+    }
+    return cnt;
+}
+
+int main(){
+    char buffer[200];
+    String frase;
+    int len;
+
+    printf("Inserisci una stringa: ");
+    fgets(buffer, sizeof(buffer), stdin);
+    len= strlen(buffer);
+    compatta(buffer, len);
+    frase = (String)malloc((len+1)*sizeof(char));
+
+    if(frase==NULL){
+        printf("C'è un errore\n");
+        return 1;
+    }
+
+    strcpy(frase, buffer);
+    printf("Il numero delle vocali è: %d", conteggio(frase));
+    free(frase);
+    return 0;
+}
