91dda779701291b8c0f305684883780de16eec96
SebastianCuoghi2008 <sebastian.cuoghi@galileiostiglia.edu.it>
2025-10-25 21:00:55 +0000

    Mistero2

---
+++ Stringhe/Mistero2.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    char **parole = NULL;
+    int np = 0;
+    char *tmp = malloc(30);
+    int i = 0;
+    char c;
+
+    while ((c = getchar()) != '\n'){
+        if (c != ' '){
+            tmp[i++] = c;
+        }else{
+            tmp[i] = '\0';
+            parole = realloc(parole, (np + 1) * sizeof(char *));
+            parole[np] = malloc(i + 1);
+            for (int k = 0; k <= i; k++)
+                parole[np][k] = tmp[k];
+            np++;
+            i = 0;
+        }
+    }
+    tmp[i] = '\0';
+    parole = realloc(parole, (np + 1) * sizeof(char *));
+    parole[np] = malloc(i + 1);
+    for(int k = 0; k <= i; k++){
+        parole[np][k] = tmp[k];
+    }
+    np++;
+
+    printf("Totale parole: %d\n", np);
+    for(int k = 0; k < np; k++){
+        printf("%s\n", parole[k]);
+    }
+    for(int k = 0; k < np; k++){
+        free(parole[k]);
+    }
+    free(parole);
+    free(tmp);
+    return 0;
+}
