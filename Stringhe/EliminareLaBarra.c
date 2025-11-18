690b9fab1bb67ed6f738bbcb8d8436c0d95fce28
SebastianCuoghi2008 <sebastian.cuoghi@galileiostiglia.edu.it>
2025-11-18 08:59:03 +0000

    EliminareLaBarra

--- Stringhe/EliminareLaBarra.c
+++ Stringhe/EliminareLaBarra.c
@@ -5,42 +5,28 @@
 #include <string.h>
 #include <ctype.h>
 
-typedef char* String;
+typedef char* Stringa;
 
-String compattaSpazi(String buffer, int len){
-    for(int i=0; i<len; i++){
-        if(buffer[i]==' '){
-            for(int j=i; j<len-1; j++){
-                buffer[j]=buffer[j+1];            
-            }
-            len--; 
-        }
-    }
-    buffer=(String)realloc(buffer, (len+1) * sizeof(char));
-    if(buffer==NULL){
-        printf("Errore di allozione memoria\n");
-        exit(1);
-    }
-    return buffer;    
+void eliminaSpazi(Stringa S){
+	int i,j;
+	for(i=0; S[i]!='\0'; i++){
+		if(S[i]==' '){
+			for(j=i; S[j] != '\0'; j++){
+				S[j]=S[j+1];
+			}			
+			S[j]='\0';
+		}
+	}
 }
+
 
 int main(){
-    char buffer[200];
-    String frase;
-    int len;
-
-    printf("Inserisci una stringa: ");
-    fgets(buffer, sizeof(buffer), stdin);
-    len= strlen(buffer);
-    compattaSpazi(buffer, len);
-    frase = (String)malloc((len+1)*sizeof(char));
-
-    if(frase==NULL){
-        printf("C'è un errore\n");
-        return 1;
-    }
-
-    strcpy(frase, buffer);
-    printf("La stringa senza spazi e': %s\n", frase);
-    free(frase);
+	Stringa S = (Stringa)malloc(50 * sizeof(char));
+	if (S == NULL) return 1;
+	
+	printf("Inserisci una stringa: ");
+	fgets(S, 50, stdin);
+	eliminaSpazi(S);
+	printf("\n%s", S);		
 }
+
---
+++ Stringhe/L'inverso.c
@@ -0,0 +1,31 @@
+/*Data una stringa, scriverla all'inverso scambiando gli elementi*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void Inverso(char* Stringa, int Lung){
+    char temp;
+    for(int i=0; i<Lung/2; i++){
+        temp=Stringa[i];
+        Stringa[i]=Stringa[Lung-i-1];
+        Stringa[Lung-i-1]=temp;
+    }
+}
+
+int main(){
+    char* Stringa=(char*)malloc(50*sizeof(char));
+    int Lung;
+    if (Stringa==NULL)
+    {
+        printf("Errore di allocazione");
+        return 1; 
+    }
+    
+    printf("inserisci una stringa: ");
+    fgets(Stringa, 50, stdin);
+    Lung=strlen(Stringa);
+    printf("La stringa inversa e': %s", Stringa);
+    free(Stringa);
+    return 0;
+}
