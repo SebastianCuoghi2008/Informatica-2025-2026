78488b22501131d7608b18207b24e07cfb9b16d1
SebastianCuoghi2008 <sebastian.cuoghi@galileiostiglia.edu.it>
2026-01-19 20:35:34 +0000

    AllenamentoStruct3

--- Struct/AllenamentoStruct3.c
+++ Struct/AllenamentoStruct3.c
@@ -13,16 +13,16 @@ Stampare a schermo il nome del proprietario con le bollette più costose, e quel
 
 Eliminare dall'array tutte le case con più di 800 euro di bollette
 
-Aggiungere una casa all'array, facendo inserire i dati all'utente.
-
-Stampare di nuovo a schermo il nome del proprietario con le bollette più costose.*/
+Aggiungere una casa all'array, facendo inserire i dati all'utente.*/
 
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 
+typedef char* Stringa;
+
 struct Casa{
-    float Costo_bollette;
+    int Costo_bollette;
     struct Proprietario{
         char Nome[30];
         char Cognome[30];
@@ -80,7 +80,7 @@ int main(){
         printf("Cognome: %s \n", Case[i].Proprietario.Cognome);
         printf("Via: %s \n", Case[i].Proprietario.Indirizzo.Via);
         printf("Livello: %d \n", Case[i].Proprietario.Indirizzo.Numero_civico);
-        printf("Livello: %f \n\n", Case[i].Costo_bollette);
+        printf("Livello: %d \n\n", Case[i].Costo_bollette);
     }
 
     //Ricerca della casa con più bollette:
@@ -119,7 +119,7 @@ int main(){
     }
 
     //Realloc per ristampare i dati:
-    Case = (struct Casa*)malloc(N * sizeof(struct Casa));
+    Case = (struct Casa*)realloc(Case, N * sizeof(struct Casa));
 
     if(Case == NULL){
         printf("Errore di allocazione");
@@ -134,7 +134,41 @@ int main(){
         printf("Cognome: %s \n", Case[i].Proprietario.Cognome);
         printf("Via: %s \n", Case[i].Proprietario.Indirizzo.Via);
         printf("Livello: %d \n", Case[i].Proprietario.Indirizzo.Numero_civico);
-        printf("Livello: %f \n\n", Case[i].Costo_bollette);
+        printf("Livello: %d \n\n", Case[i].Costo_bollette);
+    }
+
+    //Realloc per ristampare i dati:
+    N += 1;
+
+    Case = (struct Casa*)realloc(Case, N * sizeof(struct Casa));
+
+    if(Case == NULL){
+        printf("Errore di allocazione");
+        return 1;
+    }
+
+    //inserimento dato utente casa dinamica:
+    printf("Inserisci il nome del proprietario: ");
+    fgets(Case[N-1].Proprietario.Nome, 30, stdin);
+    printf("Inserisci il cognome del proprietario: ");
+    fgets(Case[N-1].Proprietario.Cognome, 30, stdin);
+    printf("Inserisci la via della casa: ");
+    fgets(Case[N-1].Proprietario.Indirizzo.Via, 50, stdin);
+    printf("Inserisci il numero civico della casa: ");
+    scanf("%d", &Case[N-1].Proprietario.Indirizzo.Numero_civico);
+    printf("Inserisci il costo delle bollette: ");
+    scanf("%d", &Case[N-1].Costo_bollette);
+
+    //Visualizzaione dei dati aggiornati:
+    printf("\n===Lista Case===\n\n");
+    for(int i = 0; i < N; i ++){
+        printf("Casa %d: \n", i + 1);
+        printf("Nome: %s \n", Case[i].Proprietario.Nome);
+        printf("Cognome: %s \n", Case[i].Proprietario.Cognome);
+        printf("Via: %s \n", Case[i].Proprietario.Indirizzo.Via);
+        printf("Livello: %d \n", Case[i].Proprietario.Indirizzo.Numero_civico);
+        printf("Livello: %d \n\n", Case[i].Costo_bollette);
     }
     free(Case);
+
 }
