7efb46402ad7211a9641b65839e55c08f4512b63
SebastianCuoghi2008 <sebastian.cuoghi@galileiostiglia.edu.it>
2026-03-17 08:56:17 +0000

    FileFunzione

--- File/FileStruct.c
+++ File/FileStruct.c
@@ -10,23 +10,26 @@ typedef struct{
 
 int main(){
     FILE *pFile = fopen("Struct.txt", "w");
-    Persona Persone[2];
+    Persona Persone[0];
 
     if(pFile == NULL){
         printf("Errore di apertura!\n");
         return 1;
     }
     
-    for(int i = 0; i < Persone; i++){
+    for(int i = 0; i < 1; i++){
         printf("Inserisci il sospetto/a:\n");
         fgets(Persone[i].Nome, 30, stdin);
+        Persone[i] = [sizeof(Persone->Nome) - 1] = '\0';
         fgets(Persone[i].Cogome, 30, stdin);
+        Persone[i] = [sizeof(Persone->Cognome) - 1] = '\0';
         scanf("%d", &Persone[i].Eta);
-        scanf("%c", &Persone[i].Targa);
-        fgets(Persone[i].Nome, 8, stdin);
+        scanf("%c", &Persone[i].Sesso);
+        fgets(Persone[i].Targa, 8, stdin);
+        Persone[i]= [sizeof(Persone.Targa) - 1] = '\0';
     }
 
-    fprintf(pFile, "%s\n %s\n %d\n %s\n %s\n", Persone->Nome, Persone->Cogome, Persone->Eta, Persone->Sesso, Persone->Targa);
+    fprintf(pFile, "%s\n %s\n %d\n %c\n %s\n", Persone->Nome, Persone->Cogome, Persone->Eta, Persone->Sesso, Persone->Targa);
 
     fclose(pFile);
     return 0;
